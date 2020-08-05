724e8f11a1b2c204c38125a10863677f700afed2
Jonatandb <jonatandb@gmail.com>
2020-08-05 11:56:11 -0300

    Little fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(urlMongo, {
 });
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ imit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 
 routes(app);
